eaeb0d8a9c5df1876bb45d7275e9f1b9420087cf
amaresh12 <amaresh.samantaray4@gmail.com>
2019-03-06 21:51:30 +0530

    Address confirmation going on

---
+++ app/src/main/java/com/order/quickfurniture/Activity/ConfirmAddress.java
@@ -0,0 +1,13 @@
+package com.order.quickfurniture.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfirmAddress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_address);
+    }
+}
---
+++ app/src/main/java/com/order/quickfurniture/Adapter/NewCartAdapter.java
@@ -0,0 +1,4 @@
+package com.order.quickfurniture.Adapter;
+
+public class NewCartAdapter {
+}
